016c2d2b07b68c20c0fc281086689f5b2c011b08
Brian <bstep1@umbc.edu>
2023-04-12 16:58:26 -0400

    disable scroll listener

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,35 +9,35 @@ export default function Navbar() {
 
   // mui has a "useScrollTrigger" hook that it recommends using for this situation,
   // but it's very functionally limited and can't be modified much.
-  useEffect(() => {
-    // This is a simple static Y value. May want to expand it such that
-    // when scrolling down y1 relative pixels it hides, and when scrolling up y2
-    // relative pixels it unhides.
-    const handleScroll = () => {
-      setIsVisible(window.scrollY < 200)
-    }
+  // useEffect(() => {
+  //   // This is a simple static Y value. May want to expand it such that
+  //   // when scrolling down y1 relative pixels it hides, and when scrolling up y2
+  //   // relative pixels it unhides.
+  //   const handleScroll = () => {
+  //     setIsVisible(window.scrollY < 200)
+  //   }
 
-    // adding "passive" is supposed to increase performance for scrolling. See:
-    // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#improving_scrolling_performance_with_passive_listeners
-    window.addEventListener('scroll', handleScroll, { passive: false })
+  //   // adding "passive" is supposed to increase performance for scrolling. See:
+  //   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#improving_scrolling_performance_with_passive_listeners
+  //   window.addEventListener('scroll', handleScroll, { passive: false })
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll)
+  //   }
+  // }, [])
 
   return (
-    <Slide appear={false} direction="down" in={isVisible}>
-      <AppBar position="sticky" sx={{ mb: 2 }}>
-        <Toolbar>
-          <NavbarDrawer />
-          <Typography variant="h5">
-            <Link href={'/'}>Iron Log</Link>
-          </Typography>
-          <Box flex={1} />
-          <LoginButton />
-        </Toolbar>
-      </AppBar>
-    </Slide>
+    // <Slide appear={false} direction="down" in={isVisible}>
+    <AppBar position="static" sx={{ mb: 2 }}>
+      <Toolbar>
+        <NavbarDrawer />
+        <Typography variant="h5">
+          <Link href={'/'}>Iron Log</Link>
+        </Typography>
+        <Box flex={1} />
+        <LoginButton />
+      </Toolbar>
+    </AppBar>
+    // </Slide>
   )
 }
