896ad171cecb5207ff9ed6f2275fba3e921cce8e
Brian <bstep1@umbc.edu>
2023-04-20 14:51:47 -0400

    setup custom nextauth middleware example

--- middleware.api.ts
+++ middleware.api.ts
@@ -1,4 +1,17 @@
+import { withAuth } from 'next-auth/middleware'
+
 // this will route to the login screen if not logged in, from any route
 // todo: might want to exclude pages/api so it can send json back instead of redirecting to sign in page
 // see: https://next-auth.js.org/configuration/nextjs#middleware
-export { default } from 'next-auth/middleware'
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      // disable sign in redirect in dev/test mode. Must also set a userId in [...nextauth]'s callback
+      if (process.env.NODE_ENV !== 'production') {
+        return true
+      }
+      // this is the default behavior
+      return !!token
+    },
+  },
+})
